eabf4bb96440b23f6436df7f3967f262edcfeda8
nemanja-djokic <djokic_nemanja@hotmail.com>
2021-01-31 16:22:47 +0100

    Updated ItemDAO interface.

--- Application/src/main/java/db/dao/ItemDAO.java
+++ Application/src/main/java/db/dao/ItemDAO.java
@@ -2,12 +2,13 @@ package db.dao;
 
 import db.dto.Item;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface ItemDAO {
-    public ArrayList<Item> GetAll();
-    public boolean createItem(Item Item);
-    public Item readItem(int id);
-    public boolean updateItem(Item Item);
-    public boolean deleteItem(int id);
+    public ArrayList<Item> GetAll() throws SQLException;
+    public boolean createItem(Item item) throws SQLException;
+    public Item readItem(int id) throws SQLException;
+    public boolean updateItem(Item item) throws SQLException;
+    public boolean deleteItem(int id) throws SQLException;
 }
