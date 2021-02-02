97233ae41c7c37a8d76946b02f861f3cb1da2f53
Bosančić Aleksandar <34287315+RockBobHardPants@users.noreply.github.com>
2021-02-02 14:52:01 +0100

    Deleted duplicate classes

--- Application/src/main/java/ItemDataAccess.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface ItemDataAccess {
-    boolean addItem(Item item);
-    Item getItem(int id);
-    List<Item> getItems(String manufacturer, String model);
-    boolean update(Item item);
-    boolean delete(Item item);
-}
--- Application/src/main/java/ReceiptDataAccess.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface ReceiptDataAccess {
-    boolean addReceipt(Receipt receipt);
-    Receipt getReceipt(int id);
-    List<Receipt> getAllReceipts();
-    boolean deleteReceipt(int id);
-}
